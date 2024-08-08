ee019b4212cf2cdd09cf4f03adbd4d7913c1efd2
Guillermo <vital_tmv@yahoo.com>
2024-08-08 15:37:09 -0500

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "Black", "Nice", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 23, "M", "Red", "intense", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 24, "F", "Silver", "Cool", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 25, "M", "Brown", "Dominant", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 21, "F", "Yellow", "Wild", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 26, "M", "Green", "Naughty", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 20, "F", "Red", "Calm", "TRUE");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, "M", "Black", "Nice", "TRUE");
